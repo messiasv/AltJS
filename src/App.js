fcff9e5e645107637bc9647c637ad70cb8644f04
Valentin Messias <mv77270@gmail.com>
2017-06-01 18:19:34 +0900

    Add result area & button

--- src/App.js
+++ src/App.js
@@ -15,11 +15,13 @@ class App extends Component {
 
 Quisque porta eget mauris nec scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim ultricies tellus, non pharetra lorem posuere eu. Nullam sollicitudin elementum nibh. Donec eu odio nisi. Vestibulum hendrerit blandit massa non tincidunt. Suspendisse pellentesque tincidunt felis mollis viverra. Nulla feugiat mi vel est tincidunt, ac mattis ex tristique. Integer facilisis justo augue, eu luctus lorem congue vitae. Phasellus mauris velit, tempor ut felis et, hendrerit malesuada lacus. Fusce at suscipit ipsum. Aliquam ac diam mi.
         </p>
+        <button id="run-button" type="Button">&#9658;</button>
         <div className="App-content">
           <div className="Code-area">
             <textarea className="Code-entry" placeholder="Please type in some code!"></textarea>
           </div>
           <div className="Result-area">
+            <textarea className="Result" disabled="disabled" placeholder="Result"></textarea>
           </div>
         </div>
       </div>
